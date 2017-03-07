302946ff6ae0ec6188cbe4f117110f979e231c98
Steven van der Merwe <stevenvdmerwe@gmail.com>
2017-03-07 22:08:09 +0200

    Added audio styler and visualizer

--- app/components/main/mainController.js
+++ app/components/main/mainController.js
@@ -1,7 +1,18 @@
 angular.module('main')
     .controller('mainController', ['$scope', function MainController($scope) {
+        // Tail = require('tail').Tail;
 
-         var audio = document.getElementById('prismatic-audio');
+        // tail = new Tail("./app.log");
+
+        // tail.on("line", function(data) {
+        //     console.log(data);
+        // });
+
+        // tail.on("error", function(error) {
+        //     console.log('ERROR: ', error);
+        // });
+
+        var audio = document.getElementById('prismatic-audio');
 
         $scope.title = 'Prismatic Player';
         $scope.youtubeDownloadLink = 'music/hiphop.mp3';
@@ -9,6 +20,10 @@ angular.module('main')
         $scope.playMusic = function() {
             audio.load();
             audio.play();
+
+            var visuals = new Visualizer();
+            visuals.ini();
+            visuals._visualize();
         };
 
         $scope.pauseMusic = function() {
